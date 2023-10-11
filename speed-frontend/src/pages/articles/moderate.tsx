c25272649eaf881803767b42d4c4ffd5859167a3
Yunsik <qkr7260@autuni.ac.nz>
2023-10-11 17:15:30 +1300

    moderate page modified

--- speed-frontend/src/pages/articles/moderate.tsx
+++ speed-frontend/src/pages/articles/moderate.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="container">
-        <h1>Moderate Submissions for the Speed Database!</h1>
+        <h1>Moderate adfasdf Submissions for the Speed Database!</h1>
         </div>
         );
 }
