72b2bb587c6d8d1f7f915f216b6a342247224b7b
KT5334 <yfq5334@autuni.ac.nz>
2023-10-22 15:18:49 +1300

    Added global CSS styles

--- speed-frontend/src/components/table/SortableTable.tsx
+++ speed-frontend/src/components/table/SortableTable.tsx
@@ -3,27 +3,27 @@ import React from "react";
 interface SortableTableProps {
   headers: { key: string; label: string }[];
   data: any[];
-  visibleColumns: string[]; // Add this line to declare the visibleColumns prop
+  visibleColumns: string[]; 
 }
 
 const SortableTable: React.FC<SortableTableProps> = ({ headers, data, visibleColumns }) => (
-  <table style={{ border: '1px solid #ccc', borderCollapse: 'collapse' }}>
-    <thead>
+  <table style={{ border: '1px solid darkgrey', borderCollapse: 'collapse' }}>
+    <thead style={{ backgroundColor: '#ad6b3a', color: 'white' }}>
       <tr>
         {headers
           .filter((header) => visibleColumns.includes(header.key)) // Filter headers based on visibility
           .map((header) => (
-            <th style={{ border: '1px solid #ccc', padding: '8px' }} key={header.key}>{header.label}</th>
+            <th style={{ border: '1px solid darkgrey', padding: '8px' }} key={header.key}>{header.label}</th>
           ))}
       </tr>
     </thead>
-    <tbody>
+    <tbody style={{ backgroundColor: '#ded7cd' }}>
       {data.map((row, i) => (
         <tr key={i}>
           {headers
             .filter((header) => visibleColumns.includes(header.key)) // Filter data based on visibility
             .map((header) => (
-              <td style={{ border: '1px solid #ccc', padding: '8px' }} key={header.key}>{row[header.key]}</td>
+              <td style={{ border: '1px solid darkgrey', padding: '8px' }} key={header.key}>{row[header.key]}</td>
             ))
           }
         </tr>
