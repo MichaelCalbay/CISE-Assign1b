1c10f362f733b17b2c7e18b92260f9cdebfeba46
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-10-24 10:10:57 +1300

    Test: main.ts update

--- speed-backend/src/main.ts
+++ speed-backend/src/main.ts
@@ -1,13 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as bodyParser from 'body-parser'
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors();
+//   app.use(bodyParser.json());
+  
+//   await app.listen(3032);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser'
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter() 
+  );
 
   app.enableCors();
-  app.use(bodyParser.json());
-  
+  app.use(json()); 
+
   await app.listen(3032);
 }
 bootstrap();
