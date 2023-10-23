4cfb71b49957b8831dac6289d665fa7bbaf7aa27
MichaelCalbay <vjc0781@autuni.ac.nz>
2023-10-24 10:19:39 +1300

    Fix: reverted main.

--- speed-backend/src/main.ts
+++ speed-backend/src/main.ts
@@ -1,31 +1,13 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import * as bodyParser from 'body-parser'
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.enableCors();
-//   app.use(bodyParser.json());
-  
-//   await app.listen(3032);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { json } from 'express';
+import * as bodyParser from 'body-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter() 
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.use(json()); 
-
+  app.use(bodyParser.json());
+  
   await app.listen(3032);
 }
 bootstrap();
